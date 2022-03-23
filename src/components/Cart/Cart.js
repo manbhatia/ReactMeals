e702cdbd633f711cef378959726e60f9a6922f5e
Manohar Bhatia <bhatia.manohar1993@gmail.com>
2022-03-23 16:05:31 +0530

    Initial commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,55 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+import Modal from '../UI/Modal';
+import styles from './Cart.module.css';
+import CartItem from './CartItem';
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
+  const cartItems = (
+    <ul className={styles['cart-items']}>
+      {cartCtx.items.map((item) => {
+        return (
+          <CartItem
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            price={item.price}
+            amount={item.amount}
+            onRemove={cartItemRemoveHandler.bind(null, item.id)}
+            onAdd={cartItemAddHandler.bind(null, item)}
+          />
+        );
+      })}
+    </ul>
+  );
+
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={styles.total}>
+        <span>Total Amount</span>
+        <span>{`$${cartCtx.totalAmount.toFixed(2)}`}</span>
+      </div>
+      <div className={styles.actions}>
+        <button className={styles['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={styles.button}>Order</button>}
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import AvailableMeals from './AvailableMeals';
+import MealsSummmay from './MealsSummary';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummmay />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
